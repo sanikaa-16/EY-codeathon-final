93440cbc6c9ab2e81d16617af9bd506f52b58685
sanika kamath <sanikakamath@sanikas-MacBook-Air.local>
2025-02-13 15:50:38 +0530

    done

--- server/run.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    with app.app_context():
-        from app import db
-        db.create_all()
-    app.run(host="0.0.0.0",debug=True, port=8080)
